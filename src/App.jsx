149d8824508ee3f4a063aa5210a03898b66323bb
puppyprogrammer <210118048+puppyprogrammer@users.noreply.github.com>
2025-07-18 01:35:53 -0500

    Installed dependancies

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-800">
+      <h1 className="text-3xl font-bold text-white">Empire’s Edge</h1>
+    </div>
+  );
+}
+
+export default App;
