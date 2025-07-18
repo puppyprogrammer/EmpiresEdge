e5cde1341ec81fa6a607f8752bc6415fcea71829
puppyprogrammer <210118048+puppyprogrammer@users.noreply.github.com>
2025-07-18 03:02:37 -0500

    Fixed Tailwind purging some css with safelist in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    'w-8',
+    'h-8',
+    'bg-green-500',
+    'bg-green-700',
+    'bg-gray-600',
+    'bg-gray-800',
+    'bg-blue-500',
+    'bg-yellow-600',
+  ],
   theme: {
     extend: {},
   },
