93b86236f5dc752303fb22bfc868e1069e06b80e
puppyprogrammer <210118048+puppyprogrammer@users.noreply.github.com>
2025-07-21 03:01:35 -0500

    Improving look of bottom menu 6

--- src/TileInformationPage.jsx
+++ src/TileInformationPage.jsx
@@ -33,13 +33,13 @@ function TileInformationPage({ selectedTile }) {
       </div>
       <div className="building-options">
         <div className="building-option" title="Build Road">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" strokeWidth="2">
             <path d="M4 12h16M12 4v16" />
           </svg>
           <span>Road</span>
         </div>
         <div className="building-option" title="Build Factory">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" strokeWidth="2">
             <rect x="2" y="6" width="20" height="12" />
             <path d="M6 6V4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v2" />
             <path d="M8 10h.01M12 10h.01M16 10h.01" />
@@ -47,7 +47,7 @@ function TileInformationPage({ selectedTile }) {
           <span>Factory</span>
         </div>
         <div className="building-option" title="Build Mine">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" strokeWidth="2">
             <path d="M12 2v6m-4-2l8 4m0-4l-8 4" />
             <rect x="2" y="8" width="20" height="14" />
           </svg>
